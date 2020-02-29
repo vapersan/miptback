89913bbc0f46da0899815bcc40a5731a72e1ac7d
reoil <imidg3001@yandex.ru>
2020-02-29 14:08:17 +0300

    Added feedback table

--- web/feedback/models.py
+++ web/feedback/models.py
@@ -10,8 +10,37 @@ from django.db import models
 # class Teacher(models.Model):
 #     pass
 
-# class Feedback(models.Model):
-#     pass
+class Feedback(models.Model):
+    class Meta:
+        verbose_name = "Отзывы об обучении"
+
+    STATUS_CODE = (
+        (0, "Не прочитано"),
+        (1, "Принятно"),
+        (2, "Отклонено"),
+    )
+
+    date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата создания",
+        blank=True
+    )
+
+    text = models.CharField(
+        max_length=1024,
+        verbose_name="Текст сообщения"
+    )
+
+    rate = models.IntegerField(
+        default=0,
+        verbose_name="Оценка сообщения"
+    )
+
+    status = models.IntegerField(
+        default=STATUS_CODE[0][0],
+        verbose_name="Статус отзыва",
+        choices=STATUS_CODE
+    )
 
 # class FeedbackMonthly(models.Model):
 #     pass
