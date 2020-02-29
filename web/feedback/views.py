15e5e3d338d322bc5544aa96c4339002a87be87a
reoil <imidg3001@yandex.ru>
2020-02-29 19:29:23 +0300

    Update alice

--- web/feedback/views.py
+++ web/feedback/views.py
@@ -1,9 +1,10 @@
 import json
 
 from django.http import JsonResponse
-
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
+
+from feedback.models import Feedback
 
 aliceSession = {}
 
@@ -14,10 +15,10 @@ def alice_skill(request):
         req_json = json.loads(request.body)
     except Exception:
         req_json = {'version': 1, 'session': 1}
-    print(request.body)
-    print(request.POST)
-    print(request.GET)
-    print(request.method)
+    uid = req_json['session']['user_id']
+    cmd = req_json['request']['command']
+    if uid not in aliceSession:
+        aliceSession[uid] = 0
     response = {
         "version": req_json['version'],
         "session": req_json['session'],
@@ -26,4 +27,16 @@ def alice_skill(request):
             'text': 'Привет :D'
         }
     }
+    if aliceSession[uid] == 0:
+        # TODO: make it sound must better
+        response['response']['text'] = 'Привет!'
+        aliceSession[uid] += 1
+    elif aliceSession[uid] == 1:
+        if len(cmd) < 20:
+            response['response']['text'] = 'Очень мало слов. Текст должен быть более 20 символов'
+        else:
+            Feedback(text=cmd).save()
+            response['response']['text'] = 'Спасибо за отзыв!'
+            response['response']['end_session'] = True
+    aliceSession[uid] = aliceSession[uid] % 2
     return JsonResponse(response, json_dumps_params={'ensure_ascii': False})
