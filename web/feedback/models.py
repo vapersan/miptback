2e3f2e8aca2f77cac70f88bfd9587e8fd3b45ea5
reoil <imidg3001@yandex.ru>
2020-02-29 13:21:54 +0300

    Added base django

---
+++ web/feedback/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+# Create your models here.
+
+# class Groups(models.Model):
+#     pass
+
+
+# class Teacher(models.Model):
+#     pass
+
+# class Feedback(models.Model):
+#     pass
+
+# class FeedbackMonthly(models.Model):
+#     pass
