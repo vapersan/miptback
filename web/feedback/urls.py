321e4d26d5fe061403025a9c8b4c37e59601cd9d
reoil <imidg3001@yandex.ru>
2020-03-01 17:14:25 +0300

    route fix

--- web/feedback/urls.py
+++ web/feedback/urls.py
@@ -6,8 +6,8 @@ from .serializer import router
 urlpatterns = [
     path('alice/', alice_skill),
     path('api/', include(router.urls)),
-    path('api_json_personalInf/', json_resp_api_1),
-    path('api_json_study/', json_resp_api_2),
+    path('api/json_personalInf/', json_resp_api_1),
+    path('api/json_study/', json_resp_api_2),
     path('api/get-monthly-event/', get_monthly_event),
     path('api/save-monthly-event/', save_monthly_event),
     path('api/monthly-get-admin-info/', monthly_get_admin_info),
